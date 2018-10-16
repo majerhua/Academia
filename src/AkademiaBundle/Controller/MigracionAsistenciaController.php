20ea2f656a80aefc32841cf1fb34626b3647cf44
cmajerhua <majerhua123@gmail.com>
2018-10-16 15:17:16 -0500

    trayendo cambios desarrollo

---
+++ src/AkademiaBundle/Controller/MigracionAsistenciaController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace AkademiaBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+use Symfony\Component\HttpFoundation\JsonResponse; 
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
+class MigracionAsistenciaController extends Controller
+{
+	public function disciplinaPrincipalAction(Request $request, $idTemporada)
+    {
+        $em = $this->getDoctrine()->getManager();
+         
+        $estadoUpdDis = NULL;
+        $usuario = $this->getUser()->getId();
+
+        $estado = $em->getRepository('AkademiaBundle:DisciplinaDeportiva')->crearConfiguracionDisciplinaIsNullTemp($idTemporada,$usuario);
+
+        $disciplinasTotalesActivas = $em->getRepository('AkademiaBundle:DisciplinaDeportiva')->getDisciplinasTotales($idTemporada);
+        $descripcionTemporada = $em->getRepository('AkademiaBundle:Temporada')->getDescripcionTemporadaById($idTemporada);
+
+        return $this->render('AkademiaBundle:Migracion_Asistencia:disciplinas.html.twig', array('estadoUpdDis'=>$estadoUpdDis,'disciplinas' => $disciplinasTotalesActivas, 'descripcionTemporada' => $descripcionTemporada , 'idTemporada'=>$idTemporada ));
+    }
+
+	public function disciplinaConfiguracionByIdAction(Request $request)
+    {
+
+    	$idDisciplina = $request->get('idDisciplina');
+        $idTemporada = $request->get('idTemporada');
+
+        $em = $this->getDoctrine()->getManager(); 
+
+       	$disciplina = $em->getRepository('AkademiaBundle:DisciplinaDeportiva')->getDisciplinaConfiguracionById($idDisciplina,$idTemporada);
+
+        $encoders = array(new JsonEncoder());
+        $normalizer = new ObjectNormalizer();
+        $normalizer->setCircularReferenceLimit(1);
+        $normalizer->setCircularReferenceHandler(function ($object) {
+            return $object->getId();
+        });
+        $normalizers = array($normalizer);
+        $serializer = new Serializer($normalizers, $encoders);
+        $jsonContent = $serializer->serialize($disciplina,'json');
+        return new JsonResponse($jsonContent);
+    }
+
+	public function updateDisciplinaAction(Request $request)
+    {
+    	
+    	$idDisciplina = $request->get('idDisciplina');
+    	$convencionalEdadMinima = $request->get('convencional-edad-minima');
+    	$convencionalEdadMaxima = $request->get('convencional-edad-maxima');
+    	$discapacitadoEdadMinima = $request->get('discapacitado-edad-minima');
+    	$discapacitadoEdadMaxima = $request->get('discapacitado-edad-maxima');
+        $idTemporada = $request->get('idTemporada');
+        
+        $usuario = $this->getUser()->getId();
+
+        $em = $this->getDoctrine()->getManager(); 
+        $estadoUpdDis = $em->getRepository('AkademiaBundle:DisciplinaDeportiva')->updateDisciplina($idDisciplina,$convencionalEdadMinima,$convencionalEdadMaxima,$discapacitadoEdadMinima,$discapacitadoEdadMaxima,$idTemporada,$usuario);
+
+        $disciplinasTotalesActivas = $em->getRepository('AkademiaBundle:DisciplinaDeportiva')->getDisciplinasTotales($idTemporada);
+
+        echo $this->renderView('AkademiaBundle:Migracion_Asistencia:table_disciplina.html.twig',array('estadoUpdDis'=>$estadoUpdDis,'disciplinas' => $disciplinasTotalesActivas,'idTemporada'=>$idTemporada));
+        exit;
+    }
+
+// public function insertAsistenciaBeneficiarios($arrayAsistenciaMensual,$idHorario,$usuario){
+
+//         $query = "  SELECT DISTINCT ins.id inscribeteId,mov.categoria_id,mov.asistencia_id
+//                     FROM ACADEMIA.horario hor 
+//                     INNER JOIN ACADEMIA.inscribete ins ON ins.horario_id = hor.id
+//                     INNER JOIN (SELECT MAX(movMax.id) idMov, movMax.inscribete_id insMov 
+//                                 FROM ACADEMIA.movimientos movMax
+//                                 GROUP BY movMax.inscribete_id
+//                                 ) maxMov  ON  maxMov.insMov = ins.id
+//                     INNER JOIN ACADEMIA.movimientos mov ON mov.id = maxMov.idMov
+//                     WHERE ins.estado = 2 AND hor.id = $idHorario";
+        
+//         $stmt = $this->getEntityManager()->getConnection()->prepare($query);
+//         $stmt->execute();
+//         $inscribeteHorario = $stmt->fetchAll();
+
+//         foreach ( $inscribeteHorario as $insHor ) {
+
+//             $codAsis = 4;
+
+//             for ( $i=0 ; $i < count($arrayAsistenciaMensual) ; $i++) { 
+
+//                 if ( $insHor['inscribeteId'] == $arrayAsistenciaMensual[$i])
+//                     $codAsis = 2;
+                
+//             }
+
+//             $categoriaId = $insHor['categoria_id'];
+//             $inscribeteId = $insHor['inscribeteId'];
+
+//             $queryInsertMovAsis = "INSERT INTO ACADEMIA.movimientos(categoria_id,asistencia_id,
+//                 inscribete_id,usuario_valida)
+//                 VALUES( $categoriaId , $codAsis , $inscribeteId , $usuario )";
+        
+//             $stmt = $this->getEntityManager()->getConnection()->prepare($queryInsertMovAsis);
+//             $stmt->execute();
+//         }
+//     }
+    
+}
---
+++ src/AkademiaBundle/Controller/TemporadaController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace AkademiaBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class TemporadaController extends Controller
+{
+
+   public function temporadaCrearAction(Request $request){
+    
+        if($request->isXmlHttpRequest()){
+
+            $crearAnio = $request->get('crear-anio');
+            $crearCiclo = $request->get('crear-ciclo');
+            $crearApertura  = $request->get('crear-apertura');
+            $crearPreInscripcion = $request->get('crear-pre-inscripcion');
+            $crearInicioClases = $request->get('crear-inicio-clases');
+            $crearCierreClases = $request->get('crear-cierre-clases');
+            $crearFechaSubsanacion = $request->get('crear-fecha-subsanacion');
+
+            $em = $this->getDoctrine()->getManager();
+            $estadoUpdTemp = NULL;
+
+            $usuario = $this->getUser()->getId();
+            
+            $codeCrearTemporada = $em->getRepository('AkademiaBundle:Temporada')->crearTemporada($crearAnio,$crearCiclo,$crearApertura,$crearPreInscripcion,$crearInicioClases,$crearCierreClases,$crearFechaSubsanacion,$usuario);
+
+            $temporadas = $em->getRepository('AkademiaBundle:Temporada')->getTemporadas();
+
+
+            if( $codeCrearTemporada == 1 ){
+
+                echo $this->renderView('AkademiaBundle:Migracion_Asistencia:table_temporada.html.twig',array('estadoUpdTemp'=>$codeCrearTemporada,'temporadas'=>$temporadas));
+                exit;
+
+            }else
+                return new JsonResponse($codeCrearTemporada);
+            
+        }
+    }
+
+   public function temporadaModificarAction(Request $request){
+    
+        if($request->isXmlHttpRequest()){
+
+        	$idTemporada = $request->get('id-temporada');
+            $editarAnio = $request->get('editar-anio');
+            $editarCiclo = $request->get('editar-ciclo');
+            $editarApertura  = $request->get('editar-apertura');
+            $editarPreInscripcion = $request->get('editar-pre-inscripcion');
+            $editarInicioClases = $request->get('editar-inicio-clases');
+            $editarCierreClases = $request->get('editar-cierre-clases');
+            $editarFechaSubsanacion = $request->get('editar-fecha-subsanacion');
+
+            $em = $this->getDoctrine()->getManager();
+        	$estadoUpdTemp = NULL;
+
+            $usuario = $this->getUser()->getId();
+        	
+            $codeUpdateTemporada = $em->getRepository('AkademiaBundle:Temporada')->updateTemporada($editarAnio,$editarCiclo,$editarApertura,$editarPreInscripcion,$editarInicioClases,$editarCierreClases,$editarFechaSubsanacion,$idTemporada,$usuario);
+            $temporadas = $em->getRepository('AkademiaBundle:Temporada')->getTemporadas();
+
+            if( $codeUpdateTemporada == 1 ){
+
+            	echo $this->renderView('AkademiaBundle:Migracion_Asistencia:table_temporada.html.twig',array('estadoUpdTemp'=>$estadoUpdTemp,'temporadas'=>$temporadas));
+	        	exit;
+
+            }else
+            	return new JsonResponse($codeUpdateTemporada);
+            
+        }
+    }
+
+	public function temporadaPrincipalAction(Request $request,$idTemporada){
+    
+        $em = $this->getDoctrine()->getManager(); 
+        $estadoUpdTemp = NULL;
+
+        $descripcionTemporada = $em->getRepository('AkademiaBundle:Temporada')->getDescripcionTemporadaById($idTemporada);
+        $temporadas = $em->getRepository('AkademiaBundle:Temporada')->getTemporadas();
+
+        return $this->render('AkademiaBundle:Migracion_Asistencia:temporada.html.twig', array( 'estadoUpdTemp'=>$estadoUpdTemp , 'temporadas' => $temporadas, 'descripcionTemporada' => $descripcionTemporada,'idTemporada'=>$idTemporada ));
+    }
+}
